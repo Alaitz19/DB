04ad7e2794615d48293df50acc7bf1e6aaa099ff
ToKine0 <tommaso.chinello@studenti.unipd.it>
2024-05-02 13:08:09 +0200

    Starting transactions examples

---
+++ MyJDBCProject/src/Dbtransactions/TransactionExampleTC.java
@@ -0,0 +1,53 @@
+package Dbtransactions;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Savepoint;
+import java.sql.Statement;
+
+import java.sql.CallableStatement;
+
+public class TransactionExampleTC {
+
+	/**
+	 * Get all rows from the employee
+	 */
+	public static void getEmployee() {
+		System.out.println(" ===> Get the Content of the table employee <===");
+		String querysql = "SELECT * " + "FROM employee";
+		Connection conn = null;
+		try {
+			conn = MyDBConnection.getConnection();
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(querysql);
+			// loop through the result set
+			System.out.println("     => Printing the content ");
+			while (rs.next()) {
+				System.out.println(
+						rs.getString("Fname") + "\t\t" + rs.getString("Minit") + "\t" + rs.getString("Lname")+ rs.getString("Ssn")+ rs.getString("Bdate")+ rs.getString("Address")+ rs.getString("Sex")+ rs.getString("Salary")+ rs.getString("Super_Ssn")+ rs.getString("Dno"));
+			}
+		} catch (SQLException ex) {
+			System.out.println(ex.getMessage());
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	
+	
+	public static void main(String[] args) {
+		getEmployee();
+
+	}
+
+}
